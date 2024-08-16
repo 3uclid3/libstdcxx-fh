ac5395382a15c89297317ed525ce9e60b611302b
3uclid3 <3uclid3@users.noreply.github.com>
2024-08-16 05:07:08 +0000

    Update headers [ci skip]

--- include/bits/c++config.h
+++ include/bits/c++config.h
@@ -36,7 +36,7 @@
 #define _GLIBCXX_RELEASE 15
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20240815
+#define __GLIBCXX__ 20240816
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
