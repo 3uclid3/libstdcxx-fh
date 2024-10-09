105579b347b6cf370bdcc0f05c3ad424daeb2b70
3uclid3 <3uclid3@users.noreply.github.com>
2024-10-09 05:08:22 +0000

    Update headers [ci skip]

--- include/bits/c++config.h
+++ include/bits/c++config.h
@@ -46,7 +46,7 @@
 
 // The datestamp of the C++ library in compressed ISO date format.
 #undef __GLIBCXX__ /* The testsuite defines it to 99999999 to block PCH.  */
-#define __GLIBCXX__ 20241008
+#define __GLIBCXX__ 20241009
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
