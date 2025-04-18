709868cc51d40a5ca00887fab6a33ff6a7bfcf41
3uclid3 <3uclid3@users.noreply.github.com>
2025-04-18 05:07:51 +0000

    Update headers [ci skip]

--- include/bits/c++config.h
+++ include/bits/c++config.h
@@ -42,11 +42,11 @@
 #endif
 
 // The major release number for the GCC release the C++ library belongs to.
-#define _GLIBCXX_RELEASE 15
+#define _GLIBCXX_RELEASE 16
 
 // The datestamp of the C++ library in compressed ISO date format.
 #undef __GLIBCXX__ /* The testsuite defines it to 99999999 to block PCH.  */
-#define __GLIBCXX__ 20250417
+#define __GLIBCXX__ 20250418
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
